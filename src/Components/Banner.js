2df69190b6ac0eee303f93e30a82865c657783fe
EinasMohamed <20235266@fue.edu.eg>
2026-04-25 01:38:47 +0300

    Add files via upload

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -3,32 +3,72 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { ArrowRightCircle } from 'react-bootstrap-icons';
 import headerImg from "../assets/images/header-img.svg";
+import { useState, useEffect } from "react";
 
 export const Banner = () => {
 
-  return (
-    <section className='banner' id="home">
-        <Container>
-            <Row className="align-items-center">
-                <Col xs={12} md={6} xl={7}>
-                    <span className='tagline'>
-                        Welcome to my portfolio
-                    </span>
-                    <h1>
-                        {'Hi! I am Einas'}
-                        <span className='wrap'>
-                            , a Web Developer
+    const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const [text, setText] = useState('');
+    const [delta, setDelta] = useState(300 - Math.random() * 100);
+    const [index, setIndex] = useState(1);
+    const toRotate = [ "Web Developer", "Web Designer", "UI/UX Designer" ];
+    const period = 2000;
+
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            tick();
+        }, delta);
+        return () => { clearInterval(ticker) };
+    }, [text])
+
+    const tick = () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1):
+        fullText.substring(0, text.length + 1);
+
+        setText(updatedText);
+        if (isDeleting) {
+        setDelta(prevDelta => prevDelta / 2);
+        }
+        if (!isDeleting && updatedText === fullText) {
+            setIsDeleting(true);
+            setIndex(prevIndex => prevIndex - 1);
+            setDelta(period);
+        } else if (isDeleting && updatedText === '') {
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setIndex(1);
+            setDelta(500);
+            } else {
+                setIndex(prevIndex => prevIndex + 1);
+            }
+        }
+
+    return (
+        <section className="banner" id="home">
+            <Container>
+                <Row className="align-items-center">
+                    <Col xs={12} md={6} xl={7}>
+                        <span className="tagline">
+                            Welcome to my portfolio
                         </span>
-                    </h1>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-                    </p>   
-                    <button onClick={()=> console.log('pressed')}>Let's Connect<ArrowRightCircle size={25}></ArrowRightCircle></button>
-                </Col>
-                <Col xs={12} md={6} xl={5}>
-                    <img src={headerImg} alt='header-image'></img>
-                </Col>
-            </Row>
-        </Container>
-    </section>
-  );
-}
+                        <h1>
+                            {'Hi! I am Einas '}
+                            <span className="txt-rotate" dataPeriod="1000" data-rotate='["Web Developer", "Web Designer", "UI/UX Designer"]'>
+                            <span className="wrap">{text}</span>
+                        </span>
+                        </h1>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                        </p>   
+                        <button onClick={()=> console.log('pressed')}>Let's Connect<ArrowRightCircle size={25}></ArrowRightCircle></button>
+                    </Col>
+                    <Col xs={12} md={6} xl={5}>
+                        <img src={headerImg} alt="header-image"></img>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    );
+    }
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -5,33 +5,56 @@ import logo from '../assets/images/logo.svg';
 import icon1 from '../assets/images/nav-icon1.svg';
 import icon2 from '../assets/images/nav-icon2.svg';
 import icon3 from '../assets/images/nav-icon3.svg';
+import { useState, useEffect } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function NavBar() {
+  const[activeLink, setActiveLink] = useState('home');
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    }
+
+  window.addEventListener("scroll", onScroll);
+
+  return () => window.removeEventListener("scroll", onScroll);
+  }, [])
+
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  }
+
   return (
-    <Navbar expand="md" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="/">
-          <img src={logo} alt="Logo" />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav">
-          <span className="navbar-toggler-icon"></span>
-        </Navbar.Toggle>
-        <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ms-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#skills">Skills</Nav.Link>
-          <Nav.Link href="#projects">Projects</Nav.Link>
-        </Nav>
-          <span className="navbar-text">
-            <div className="social-icon">
-              <a href="#"><img src={icon1} alt="Linkedin" ></img></a>
-              <a href="#"><img src={icon2} alt="Facebook" ></img></a>
-              <a href="#"><img src={icon3} alt="Instagram" ></img></a>
-            </div>
-            <button onClick={()=> console.log('pressed')}>Let's connect</button>
-          </span>
-        </Navbar.Collapse>
-      </Container>
+    <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
+    <Container>
+      <Navbar.Brand href="/">
+        <img src={logo} alt="Logo" />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav">
+        <span className="navbar-toggler-icon"></span>
+      </Navbar.Toggle>
+      <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="ms-auto">
+        <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbarlink' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+        <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+        <Nav.Link href="#projects"  className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+      </Nav>
+        <span className="navbar-text">
+          <div className="social-icon">
+            <a href="#"><img src={icon1} alt="Linkedin" ></img></a>
+            <a href="#"><img src={icon2} alt="Facebook" ></img></a>
+            <a href="#"><img src={icon3} alt="Instagram" ></img></a>
+          </div>
+          <button onClick={()=> console.log('pressed')}>Let's connect</button>
+        </span>
+      </Navbar.Collapse>
+    </Container>
     </Navbar>
   );
 }
